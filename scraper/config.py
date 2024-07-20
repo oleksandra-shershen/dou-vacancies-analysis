23d6c946df4e6d7ef26adcf95e40a1da455fdc7a
Shershen Alexandra <hornet240204@gmail.com>
2024-07-20 15:34:16 +0300

    Implement scraper, add config

---
+++ scraper/config.py
@@ -0,0 +1,164 @@
+VACANCIES_URL = "https://jobs.dou.ua/vacancies/?category=Python"
+
+TECHNOLOGIES = [
+    # Programming Languages
+    "Python",
+    "SQL",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "Ruby",
+
+    # Web Frameworks
+    "Django",
+    "Flask",
+    "FastAPI",
+    "Pyramid",
+    "Tornado",
+    "Bottle",
+    "Web2py",
+
+    # Databases
+    "PostgreSQL",
+    "MySQL",
+    "SQLite",
+    "MongoDB",
+    "Redis",
+    "MariaDB",
+    "Cassandra",
+    "Elasticsearch",
+    "Neo4j",
+    "Oracle DB",
+
+    # Frontend Frameworks and Libraries
+    "React",
+    "Vue.js",
+    "Angular",
+    "Bootstrap",
+    "jQuery",
+    "Svelte",
+
+    # API Development and Documentation
+    "Django REST Framework",
+    "DRF",
+    "FastAPI",
+    "Swagger",
+    "Postman",
+    "Flask",
+    "REST API",
+    "GraphQL",
+    "gRPC",
+
+    # Version Control and CI/CD
+    "Git",
+    "GitHub",
+    "GitLab",
+    "Bitbucket",
+    "Jenkins",
+    "Travis CI",
+    "CircleCI",
+    "Azure DevOps",
+    "TeamCity",
+    "Bamboo",
+
+    # Virtualization and Containerization
+    "Docker",
+    "Kubernetes",
+    "Vagrant",
+    "VirtualBox",
+    "OpenShift",
+    "LXC/LXD",
+
+    # Cloud Services and Platforms
+    "AWS",
+    "GCP",
+    "Azure",
+    "Heroku",
+    "DigitalOcean",
+    "IBM Cloud",
+    "Oracle Cloud",
+    "Firebase",
+    "Netlify",
+    "Vercel",
+
+    # Testing
+    "pytest",
+    "unittest",
+    "Selenium",
+    "coverage.py",
+    "Mock",
+    "tox",
+    "Nose",
+    "Robot Framework",
+    "Cucumber",
+    "Behave",
+
+    # DevOps and Monitoring
+    "Ansible",
+    "Terraform",
+    "Prometheus",
+    "Grafana",
+    "Elasticsearch",
+    "Logstash",
+    "Kibana",
+    "Puppet",
+    "Chef",
+    "Nagios",
+    "Zabbix",
+    "Splunk",
+
+    # Data Science and Machine Learning
+    "NumPy",
+    "Pandas",
+    "Matplotlib",
+    "SciPy",
+    "Scikit-learn",
+    "TensorFlow",
+    "Keras",
+    "PyTorch",
+    "Jupyter Notebooks",
+    "NLTK",
+    "SpaCy",
+    "Gensim",
+    "OpenCV",
+    "Plotly",
+
+    # Web Scraping
+    "BeautifulSoup",
+    "Scrapy",
+    "Selenium",
+    "Requests",
+
+    # Message Brokers
+    "RabbitMQ",
+    "Apache Kafka",
+    "Celery",
+    "ActiveMQ",
+    "Redis Queue",
+    "ZeroMQ",
+
+    # Security
+    "OAuth",
+    "JWT",
+    "JSON Web Tokens",
+    "OpenID Connect",
+    "SSL/TLS",
+    "HTTPS",
+    "SAML",
+    "LDAP",
+    "Cryptography",
+
+    # Miscellaneous
+    "Regular Expressions",
+    "Asyncio",
+    "Celery",
+    "Multiprocessing",
+    "Threading",
+    "Subprocess",
+    "Fabric",
+    "Paramiko",
+    "WTForms",
+    "Flask-WTF",
+    "PyQt",
+    "Kivy"
+]
